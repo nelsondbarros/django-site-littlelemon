5d213a6838cd417be8a1dd13e80326d893dcc41a
Nelson de Barros <nelsinho.527@gmail.com>
2023-02-21 09:56:48 -0300

    formatting

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,20 +2,19 @@ from django.db import models
 
 
 class Booking(models.Model):
-   first_name = models.CharField(max_length=200)    
-   last_name = models.CharField(max_length=200)
-   guest_number = models.IntegerField()
-   comment = models.CharField(max_length=1000)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    guest_number = models.IntegerField()
+    comment = models.CharField(max_length=1000)
 
-   def __str__(self):
-      return self.first_name + ' ' + self.last_name
-   
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Menu(models.Model):
-   name = models.CharField(max_length=200)
-   price = models.IntegerField()
-   description = models.TextField(max_length=1000, default="")
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField(max_length=1000, default="")
 
-   def __str__(self):
-      return self.name
-
+    def __str__(self):
+        return self.name
